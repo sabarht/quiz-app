1d2c0e6a8795abf8629f114242845ee7b75433b8
Saba <sabarht@gmail.com>
2023-06-06 10:02:10 +0200

    adding form page

---
+++ index.js
@@ -0,0 +1,15 @@
+const bookmarkButton = document.querySelector(".card-bookmark");
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.textContent = "meow";
+  // bookmarkButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="490" height="40" id="bookmark"><path d="M1420 128q23 0 44 9 33 13 52.5 41t19.5 62v1289q0 34-19.5 62t-52.5 41q-19 8-44 8-48 0-83-32l-441-424-441 424q-36 33-83 33-23 0-44-9-33-13-52.5-41t-19.5-62V240q0-34 19.5-62t52.5-41q21-9 44-9h1048z"></path></svg>
+  // `;
+});
+// const answerButton = document.querySelector('[data-js="answer-button"]');
+
+// bookmarkButton.addEventListener("click", () => {
+// });
+// function bookmarked() {
+//   bookmarked = true;
+//   bookmarkButton.id = "bookmarked";
+// }
+// bookmarkButton.add;
